818e064db93f67b25209785bc6e0efdeafb0a997
Saroj <saroj@noveltytechnology.com>
2023-06-24 09:28:54 +0545

    First commit

---
+++ migrations/1687577992742-update_user_type.ts
@@ -0,0 +1,17 @@
+import { IMigration, DB } from 'migrations-mongodb'
+
+class Migration implements IMigration {
+  /**
+   * Run the migrations.
+   */
+  async up(db: DB) {
+    throw new Error('asd')
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  async down(db: DB) {}
+}
+
+export default Migration
